b23b87715a25333d740997a7593b50346080c901
Jocelyn Le Sage <jocelyn@le-sage.com>
2026-06-03 13:25:49 -0400

    Upgraded baseimage to version 4.12.3.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export HOME=/config
-
 # Added to avoid the following error message:
 #   MESA-LOADER: failed to open swrast: Error loading shared library
 #   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
